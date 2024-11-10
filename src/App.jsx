69823ec99a65c19514c9675efe5b3706d3df0f7d
abdullahibbtwd <abdullahibbtwd@gmail.com>
2024-11-10 07:12:25 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import Navbar from './component/Navbar/Navbar'
+import Hero from './component/Hero/Hero'
+import Services from './component/Services/Services'
+import Banner from './component/Banner/Banner'
+import Facilities from './component/Facilities/Facilities'
+import Campus from './component/Campus/Campus'
+import Contact from './component/Contact/Contact'
+import Footer from './component/Footer/Footer'
+import Login from "./component/Login/Login"
+import { createBrowserRouter, Routes, Route, RouterProvider, BrowserRouter, } from 'react-router-dom';
+import Register from './component/Register/Register'
+
+const Layout = () => {
+  return (
+    <>
+    <section>
+      <Hero/>
+    </section>
+    <section id="/services">
+        <Services />
+    </section>
+    <section>
+      <Banner/>
+    </section>
+      <section>
+        <Facilities/> 
+      </section>
+     <section>
+      <Campus/>
+     </section>
+     <section>
+      <Contact/>
+     </section>
+      <section>
+        <Footer />
+      </section>
+      
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/register",
+    element:<Register/>
+  },
+  {
+    path:"/Dashboard",
+    
+  },
+
+
+])
+
+const App = () => {
+  return (
+    <main className='overflow-x-hidden bg-white text-dark'>
+      <RouterProvider router={router}  basename="/" />
+    </main>
+  )
+}
+
+export default App
